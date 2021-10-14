a1e0a7871dd5b4bb30cbf59038184f6d03dbcc3a
JhonLloyd <jhonlloydgellica15@gmail.com>
2021-10-14 18:57:05 +0800

    fixed particles js error

--- Js/compile.js
+++ Js/compile.js
@@ -1,7 +1,3 @@
-particlesJS.load("particles-js", "particles.json", function () {
-  console.log("callback - particles.js config loaded");
-});
-
 let typing = new Typed(".animate", {
   strings: ["Programmer", "Designer"],
   typeSpeed: 100,
